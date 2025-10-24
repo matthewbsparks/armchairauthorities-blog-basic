00027ef55a4065eb129962b7f226ed73589134ad
Matthew Sparks <msparks1@stedwards.edu>
2025-10-23 19:04:00 -0500

    Fixes for vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
   integrations: [mdx()],
 });
