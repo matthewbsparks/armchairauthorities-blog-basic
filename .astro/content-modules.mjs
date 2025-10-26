28a52c55891d461eb7d6ce2787ecb1706a69d118
Matthew Sparks <msparks1@stedwards.edu>
2025-10-26 03:59:04 -0500

    More writing and edits

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,8 +1,8 @@
 
 export default new Map([
-["src/content/blog/content-workflow.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcontent-workflow.mdx&astroContentModuleFlag=true")],
+["src/content/blog/epstein-draft.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fepstein-draft.mdx&astroContentModuleFlag=true")],
+["src/content/blog/epstein-vs-maga.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fepstein-vs-maga.mdx&astroContentModuleFlag=true")],
 ["src/content/blog/hagiography-ck.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhagiography-ck.mdx&astroContentModuleFlag=true")],
 ["src/content/page/author.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpage%2Fauthor.mdx&astroContentModuleFlag=true")],
-["src/content/blog/epstein-vs-maga.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fepstein-vs-maga.mdx&astroContentModuleFlag=true")],
-["src/content/blog/epstein-draft.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fepstein-draft.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/content-workflow.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcontent-workflow.mdx&astroContentModuleFlag=true")]]);
 		
